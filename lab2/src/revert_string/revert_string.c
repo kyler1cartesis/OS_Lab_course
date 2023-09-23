f8124f31e7ad6ef4ef5eeca648c6f892df644db7
kyler1cartesis <87242609+kyler1cartesis@users.noreply.github.com>
2023-09-23 11:22:17 +0000

    lab 2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,22 @@
 #include "revert_string.h"
 
-void RevertString(char *str)
-{
-	// your code here
+void RevertString(char *str) {
+	if (*str == '\0') return;
+	char* iter = str;
+	// unsigned len = 0;
+	while (*(++iter) != '\0');
+	--iter;	
+	// char* reversedString = malloc(sizeof(char)*++len);
+	// iter -= 2;
+	// for (str = reversedString + len; str != reversedString;) {
+	// 	*str-- = *iter--;
+	// }
+	while (iter - str > 1) {
+		char tmp;
+		tmp = *iter;
+		*iter = *str;
+		*str = tmp;
+		++iter; --str;
+	}	
 }
 
